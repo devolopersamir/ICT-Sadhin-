69c4126d8b643932210642e82d11b33d3d44beec
Samir Talukder Apurbo <154772869+devolopersamir@users.noreply.github.com>
2025-04-12 08:15:45 +0600

    Create common.js

---
+++ common.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9OHn_xF7Z218clygJm-tvGl9bE0FvbEY",
+  authDomain: "ictsadhin-ff4d9.firebaseapp.com",
+  projectId: "ictsadhin-ff4d9",
+  appId: "1:420661343314:web:ddadf4427af520a9b9db8b"
+};
+firebase.initializeApp(firebaseConfig);
